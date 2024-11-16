7a106f226072cd42cffc22144c159c7b57c38cb9
Dawson-Myers <dawsonmyers98@gmail.com>
2024-11-15 22:44:19 -0700

    Intializing Repo with baseline info

---
+++ coffee_machine.py
@@ -0,0 +1,38 @@
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+
+
+
+def format_menu(menu_item):
+    item_espresso = MENU[menu_item]["espresso"]
+    item_latte = MENU[menu_item]["latte"]
+    item_coffee = MENU[menu_item]["coffee"]
