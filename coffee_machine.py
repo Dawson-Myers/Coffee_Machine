21d7ea5877e649bf92ba7d29e5285001d8705e06
Dawson-Myers <dawsonmyers98@gmail.com>
2024-11-17 00:20:14 -0700

    seperated Menu and Resources into seperate files and imported into coffee_machine.py

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,35 +1,6 @@
-MENU = {
-    "espresso": {
-        "ingredients": {
-            "water": 50,
-            "coffee": 18,
-        },
-        "cost": 1.5,
-    },
-    "latte": {
-        "ingredients": {
-            "water": 200,
-            "milk": 150,
-            "coffee": 24,
-        },
-        "cost": 2.5,
-    },
-    "cappuccino": {
-        "ingredients": {
-            "water": 250,
-            "milk": 100,
-            "coffee": 24,
-        },
-        "cost": 3.0,
-    }
-}
+from menu import MENU
 
-resources = {
-    "water": 300,
-    "milk": 200,
-    "coffee": 100,
-}
-
+from resources import resources
 
 
 def format_menu(menu_item):
